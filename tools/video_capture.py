e200b14085e6175f18801b979f34b4045e3b4a0c
cullen_sun_pi <cullen@raspberry.pi>
2021-05-15 01:15:50 +0800

    captured videos

--- tools/video_capture.py
+++ tools/video_capture.py
@@ -9,7 +9,7 @@ def main():
         camera.resolution = (1280, 720)
         camera.framerate = 60
         camera.start_preview()
-        camera.start_recording('../recording/highres.h264')
+        camera.start_recording('highres.h264')
        # camera.start_recording('lowres.h264', splitter_port=2, resize=(320, 240))
         camera.wait_recording(30)
         # camera.stop_recording(splitter_port=2)
