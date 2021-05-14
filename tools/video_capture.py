c45f555105defcfc413042a0dc019db552a8c047
Cullen Sun <sunhongshuai@gmail.com>
2021-05-15 01:39:05 +0800

    capture video in the desktop folder

--- tools/video_capture.py
+++ tools/video_capture.py
@@ -1,19 +1,22 @@
-import time
 import picamera
+import os
+import datetime as dt
 
 def main():    
-    # while True:
-    #     time.sleep(6)
+    file_directory = '/home/pi/Desktop/videos'
     print('Hello fish!')
     with picamera.PiCamera() as camera:
         camera.resolution = (1280, 720)
         camera.framerate = 60
         camera.start_preview()
-        camera.start_recording('highres.h264')
-       # camera.start_recording('lowres.h264', splitter_port=2, resize=(320, 240))
+
+        filename = os.path.join(file_directory, dt.datetime.now().strftime('%Y-%m-%d_%H.%M.%S.h264'))
+        camera.start_recording(filename)
+        # camera.start_recording('lowres.h264', splitter_port=2, resize=(320, 240))
         camera.wait_recording(30)
         # camera.stop_recording(splitter_port=2)
         camera.stop_recording()
+
         camera.stop_preview()
 
 if __name__ == '__main__':
