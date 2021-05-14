a50ccb255a1f0940eb9465b0edc1a00ba39292c7
Cullen Sun <sunhongshuai@gmail.com>
2021-05-15 01:08:22 +0800

    test video recording

--- tools/video_capture.py
+++ tools/video_capture.py
@@ -1,9 +1,20 @@
 import time
+import picamera
 
 def main():    
-    while True:
-        time.sleep(6)
-        print('Hello fish!')
+    # while True:
+    #     time.sleep(6)
+    print('Hello fish!')
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1280, 720)
+        camera.framerate = 60
+        camera.start_preview()
+        camera.start_recording('../recording/highres.h264')
+       # camera.start_recording('lowres.h264', splitter_port=2, resize=(320, 240))
+        camera.wait_recording(30)
+        # camera.stop_recording(splitter_port=2)
+        camera.stop_recording()
+        camera.stop_preview()
 
 if __name__ == '__main__':
     main()
