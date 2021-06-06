75fbf61b6f9ca7ca177d8f89e5ff0d4f3bdaa925
Cullen Sun <sunhongshuai@gmail.com>
2021-06-07 00:25:54 +0800

    use compressed image

--- vision/src/obstacle_detector.py
+++ vision/src/obstacle_detector.py
@@ -13,19 +13,19 @@ import sys
 import cv2
 import rospy
 
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
 
 class ObstacleDetector:
 
     def __init__(self):
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/raspicam_node/image", Image, self.callback)
-        print("Subscribed to topic /raspicam_node/image")
+        self.image_sub = rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, self.callback)
+        print("Subscribed to topic /raspicam_node/image/compressed")
 
     def callback(self, data):
         try:
-            cv_image = self.bridge.imgmsg_to_cv2(data)
+            cv_image = self.bridge.compressed_imgmsg_to_cv2(data)
         except CvBridgeError as e:
             print(e)
 
