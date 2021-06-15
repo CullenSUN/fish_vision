3dbf4d14d33244f7eb9abcaea826c4b3ddc6757d
Cullen Sun <sunhongshuai@gmail.com>
2021-06-16 02:03:49 +0800

    remove useless code

--- debug_node/src/vision_debugger.py
+++ debug_node/src/vision_debugger.py
@@ -19,11 +19,6 @@ from opencv_apps.msg import RectArray
 #from opencv_apps.msg import Rect
 from cv_bridge import CvBridge, CvBridgeError
 
-def rect_contains(rect1, rect2):
-    x1, y1, w1, h1 = rect1
-    x2, y2, w2, h2 = rect2
-    return x1 <= x2 and y1 <= y2 and x1+w1 >= x2+w2 and y1+h1 >= y2+h2 
-
 WINDOW_NAME = "BREED Fish Vision"
 
 class VisionDebugger:
