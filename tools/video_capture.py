dbae80bddc55d1ec9216dc58f30740eb89bfb636
Cullen Sun <sunhongshuai@gmail.com>
2021-07-14 00:07:14 +0800

    improve video capture video

--- tools/video_capture.py
+++ tools/video_capture.py
@@ -3,25 +3,28 @@ import os
 import datetime as dt
 from pathlib import Path
 
-file_directory = '/home/pi/Desktop/videos'
-max_num_files = 5
+def create_dir_if_not_exist(dir_path):
+    Path(dir_path).mkdir(parents=True, exist_ok=True)
 
-def delete_old_files():
-    paths = sorted(Path(file_directory).iterdir(), key=os.path.getmtime, reverse=True)
+def delete_old_files(dir_path, max_num_files=5):
+    paths = sorted(Path(dir_path).iterdir(), key=os.path.getmtime, reverse=True)
     for file in paths[max_num_files-1:]:
         print(f'removing file at {file}')
         os.remove(file)
 
-def main():    
+def capture_videos(dir_path):
+    create_dir_if_not_exist(dir_path)
+
     with picamera.PiCamera(resolution=(1280, 720), framerate=60) as camera:
         while True:
-            delete_old_files()
+            delete_old_files(dir_path)
 
-            filename = os.path.join(file_directory, dt.datetime.now().strftime('%Y-%m-%d_%H.%M.%S.h264'))
+            filename = os.path.join(dir_path, dt.datetime.now().strftime('%Y-%m-%d_%H.%M.%S.h264'))
             camera.start_recording(filename)
             camera.wait_recording(30)
             camera.stop_recording()
 
-
 if __name__ == '__main__':
-    main()
+    file_directory = '/home/pi/Desktop/videos'
+    create_dir_if_not_exist(file_directory)
+    capture_videos(file_directory, max_num_files=5)
