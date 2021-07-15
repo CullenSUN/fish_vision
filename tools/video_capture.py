30160b18b6754aa85f333d1aafc977bd258958a3
Cullen Sun <sunhongshuai@gmail.com>
2021-07-15 23:35:18 +0800

    improve video capture tool with led

--- tools/video_capture.py
+++ tools/video_capture.py
@@ -11,13 +11,21 @@ Captured videos will be saved at path `/home/pi/Desktop/videos`. Maximum 20 vide
 """
 import picamera
 import os
+import time
 import datetime as dt
 from pathlib import Path
+import RPi.GPIO as GPIO
 
-MAX_NUMBER_OF_FILES = 20
+MAX_NUMBER_OF_FILES = 100
+LED_PIN = 12
 
 def create_dir_if_not_exist(dir_path):
     Path(dir_path).mkdir(parents=True, exist_ok=True)
+
+def setup_led():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
+    GPIO.setup(LED_PIN, GPIO.OUT)
 
 def delete_old_files(dir_path):
     paths = sorted(Path(dir_path).iterdir(), key=os.path.getmtime, reverse=True)
@@ -25,13 +33,29 @@ def delete_old_files(dir_path):
         print(f'removing file at {file}')
         os.remove(file)
 
+def flash_led():
+    print("LED turning on.")
+    GPIO.output(LED_PIN, GPIO.HIGH)
+    time.sleep(0.5)
+    print("LED turning off.")
+    GPIO.output(LED_PIN, GPIO.LOW) 
+    time.sleep(0.5)
+
+    GPIO.output(LED_PIN, GPIO.HIGH)
+    time.sleep(0.5)
+    print("LED turning off.")
+    GPIO.output(LED_PIN, GPIO.LOW) 
+
+
 def capture_videos(dir_path):
     create_dir_if_not_exist(dir_path)
+    setup_led()
 
     with picamera.PiCamera(resolution=(1280, 720), framerate=60) as camera:
         while True:
             delete_old_files(dir_path)
-
+            flash_led()
+            
             filename = os.path.join(dir_path, dt.datetime.now().strftime('%Y-%m-%d_%H.%M.%S.h264'))
             camera.start_recording(filename)
             camera.wait_recording(30)
