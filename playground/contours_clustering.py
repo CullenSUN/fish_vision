74d929501870fee03fa51a4b1ba505782fb811c0
Cullen Sun <sunhongshuai@gmail.com>
2021-08-16 01:40:25 +0800

    add source code for contours clustering

---
+++ playground/contours_clustering.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import os
+import cv2
+import numpy
+
+def detect_contours(img):
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    equalized_img = cv2.equalizeHist(gray_img)
+    blurred_img = cv2.GaussianBlur(equalized_img, (9,9), 0)
+    edges = cv2.Canny(blurred_img, 90, 180)
+    _, thresh = cv2.threshold(edges, 127, 255, 0)
+    img2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_KCOS)
+    return contours
+
+def calculate_contour_distance(contour1, contour2): 
+    x1, y1, w1, h1 = cv2.boundingRect(contour1)
+    c_x1 = x1 + w1/2
+    c_y1 = y1 + h1/2
+
+    x2, y2, w2, h2 = cv2.boundingRect(contour2)
+    c_x2 = x2 + w2/2
+    c_y2 = y2 + h2/2
+
+    return max(abs(c_x1 - c_x2) - (w1 + w2)/2, abs(c_y1 - c_y2) - (h1 + h2)/2)
+
+def merge_contours(contour1, contour2):
+    return numpy.concatenate((contour1, contour2), axis=0)
+
+def agglomerative_cluster(contours, threshold_distance=40.0):
+    current_contours = contours
+    while len(current_contours) > 1:
+        min_distance = None
+        min_coordinate = None
+
+        for x in range(len(current_contours)-1):
+            for y in range(x+1, len(current_contours)):
+                distance = calculate_contour_distance(current_contours[x], current_contours[y])
+                if min_distance is None:
+                    min_distance = distance
+                    min_coordinate = (x, y)
+                elif distance < min_distance:
+                    min_distance = distance
+                    min_coordinate = (x, y)
+
+        if min_distance < threshold_distance:
+            # merge closest two contours
+            index1, index2 = min_coordinate
+            current_contours[index1] = merge_contours(current_contours[index1], current_contours[index2])
+            del current_contours[index2]
+        else: 
+            break
+
+    return current_contours
+
+if __name__ == '__main__':
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    img_path = os.path.join(script_path, 'images/pool_image.png')
+    img = cv2.imread(img_path)
+
+    contours = detect_contours(img)
+    agglomerative_cluster = agglomerative_cluster(contours)
+
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+    cv2.imshow('Detected Contours', img)
+
+    if cv2.waitKey(0):
+        cv2.destroyAllWindows()
